4dd4a5555b5bc0051d8197974a0207d5e70749c2
Guo Y.K <hi@guoyk.net>
2021-04-06 16:55:38 +0800

    update

--- util_test.go
+++ util_test.go
@@ -10,6 +10,8 @@ func TestSortProd(t *testing.T) {
 		"info-prod-2021-03-01",
 		"access-prod-2021-03-02",
 		"access-prod-2021-03-01",
+		"warn-prod-2021-03-02",
+		"warn-prod-2021-03-01",
 		"access-test-2021-03-02",
 		"access-test-2021-03-01",
 	}
